c809d4af7d2f2618f60e4b607470613b70347d60
Ralph <dizwe2716@gmail.com>
2021-05-08 15:20:32 +0900

    Add Movie Service

---
+++ src/movies/entities/movie.entity.ts
@@ -0,0 +1,7 @@
+// 원래는 database!
+export class Movie {
+  id: number;
+  title: string;
+  year: number;
+  genres: string[];
+}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,15 +1,23 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
 
 @Controller('movies') // basic Router
 export class MoviesController {
+  constructor(readonly moviesService: MoviesService) {}
+
   @Get()
   getAll() {
-    return 'This will return all movies';
-  }
-
-  @Get('/:id')
-  getOne(@Param('id') movie_id: string) {
-    return `This Will return one Movie with ${movie_id}`;
+    return this.moviesService.getAll();
   }
 
   @Get('search')
@@ -17,24 +25,31 @@ export class MoviesController {
     return `We are searching... ${searchingYear}`;
   }
 
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    const movie = this.moviesService.getOne(movieId);
+    if(!movie){
+      throw new NotFoundException(`movie with ${movieId} Not Found`);
+    }
+    return movie;
+  }
+
+  // 201 Created 가 뜸1
   @Post()
   create(@Body() movieData) {
     // Body를 받아야 하면 Body라고 명시해야 함(express와 매우 유사)
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
   remove(@Param('id') movieId: string) {
     // 여기서 param은 decorater
-    return 'This will delete a movie';
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch('/:id')
   patch(@Param('id') movieId: string, @Body() updateData) {
     // 순서대로 들어가서 밑에 ... 쓰면 key Override 됨!
-    return {
-      updatedMovie: movieId,
-      ...updateData,
-    };
+    return this.moviesService.update(movieId, updateData);
   }
 }
